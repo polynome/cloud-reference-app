5de0b65d3007bbd3a47b90d35470b9ccdcc7aebb
Bradley Buda <bradleybuda@gmail.com>
2016-06-02 13:33:49 -0700

    Working JDBC connection

--- src/main/java/co/polynome/Application.java
+++ src/main/java/co/polynome/Application.java
@@ -1,18 +1,46 @@
 package co.polynome;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class Application {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        final DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        return dataSourceBuilder.build();
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-      return "Hello World!";
+    String home() throws SQLException {
+        List<Map<String, Object>> result =  jdbcTemplate.queryForList("SELECT 1 + 2 as total");
+        return "Hello World: " + result.get(0).get("total");
     }
 
     public static void main(String[] args) {

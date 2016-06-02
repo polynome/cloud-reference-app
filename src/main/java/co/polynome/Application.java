5fe491000db341f4f0cb7f86d88467422001a737
Bradley Buda <bradleybuda@gmail.com>
2016-06-02 12:19:01 -0700

    Rename

--- src/main/java/co/polynome/Application.java
+++ src/main/java/co/polynome/Application.java
@@ -1,4 +1,4 @@
-package com.example;
+package co.polynome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
-public class DemoApplication {
+public class Applicaion {
 
     @RequestMapping("/")
     @ResponseBody
